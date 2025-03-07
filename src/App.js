e609b7c4a650557d6be5e0d4ce6956633d0de5ce
thegwanj <93459099+thegwanj@users.noreply.github.com>
2025-03-06 21:22:02 -0800

    Lab steps (and extra credit) completed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,58 @@
+import {useEffect, useState} from "react";
+import Cards from "./components/Cards";
+import CardDetail from "./components/CardDetail";
 import "./App.css";
 
+// Code by Gwanju Chung and Hala Shihab
+
 function App() {
+  const [characters, setCharacters] = useState([]);
+  const [page, setPage] = useState(1);
+  const [selectedCharacter, setSelectedCharacter] = useState(null);
+
+  useEffect(() => {
+    fetchCharacters(page);
+  }, [page]);
+
+  useEffect(() => {
+    console.log(selectedCharacter)
+  }, [selectedCharacter])
+
+  const fetchCharacters = async (page) => {
+    let response = await fetch(`https://swapi.dev/api/people/?page=${page}`);
+    let data = await response.json();
+    setCharacters(data.results);
+  };
+
+  const fetchCharacterDetails = (characterName) => {
+    const character = characters.find(c => c.name === characterName);
+    setSelectedCharacter(character)
+  };
+
+  const handleBack = () => {
+    if(page > 1){
+      setPage((prevPage) => prevPage - 1);
+      setSelectedCharacter(null);
+    };
+  };
+
+  const handleNext = () => {
+    setPage((prevPage) => prevPage + 1);
+    setSelectedCharacter(null);
+  };
+
   return (
     <div className="App">
       <h1>Star Wars Characters</h1>
       <div className="main-container ">
-        <div clssName>
+        <div className>
+          <Cards characters={characters} onCharacterClick={fetchCharacterDetails} />
           <div>
-            <button>Back</button>
-            <button>Next</button>
+            <button onClick={handleBack} disabled={page === 1}>Back</button>
+            <button onClick={handleNext} disabled={page === 9}>Next</button>
           </div>
         </div>
+        {selectedCharacter && <CardDetail character={selectedCharacter}/>}
       </div>
     </div>
   );
